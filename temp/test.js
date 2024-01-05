78b100960d33fb28671629246f09dda35e11e228
SergeiVasilyev <88876877+SergeiVasilyev@users.noreply.github.com>
2024-01-05 13:18:11 +0200

    test update

--- temp/test.js
+++ temp/test.js
@@ -7,13 +7,13 @@ var assert = require('assert');
 let url = 'https://jsonplaceholder.typicode.com/todos/1?a=25&b=10';
 let params = { c: 5, d: 35, a: 3 };
 let del_params = ['s'];
-const MyFunc = () => { return 'test'; };
+// const MyFunc = () => { return 'test'; };
 
 const url_params = new URLEditor(url);
 try {
-    console.log(url_params.add_params([MyFunc]).href);
-    console.log(decodeURIComponent(url_params.add_params([MyFunc]).href));
-    console.log(url_params.add_params([MyFunc]).searchParams);
+    console.log(url_params.add_params(params).href);
+    // console.log(decodeURIComponent(url_params.add_params([MyFunc]).href));
+    // console.log(url_params.add_params([MyFunc]).searchParams);
 } catch (error) {
     console.log(error);
 }
